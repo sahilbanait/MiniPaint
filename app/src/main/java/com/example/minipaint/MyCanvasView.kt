8f4a9d3ffc3e54cbdb4884202c1e2de311f9be10
sahilbanait <77091616+sahilbanait@users.noreply.github.com>
2021-10-25 20:44:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/minipaint/MyCanvasView.kt
@@ -0,0 +1,106 @@
+package com.example.minipaint
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
+import android.view.View
+import androidx.core.content.res.ResourcesCompat
+
+
+
+private const val STROKE_WIDTH = 12f // has to be float
+
+
+class MyCanvasView(context: Context?) : View(context){
+
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+
+
+    // Set up the paint with which to draw.
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        // Paint style specifies if the primitive being drawn is filled, stroked or both
+        style = Paint.Style.STROKE // default: FILL
+
+        strokeJoin = Paint.Join.ROUND // default: MITER
+
+        strokeCap = Paint.Cap.ROUND // default: BUTT
+
+        strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+    }
+
+    /**
+     * it defines what is being drawn
+     */
+    private var path = Path()
+
+
+    /**
+     * onSizeChanged() is called whenever a view changes the size, it is ideal place to create and setup the views canvas
+     */
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        /**
+                This method recyle the bitmap before it created
+         */
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
+
+    /**
+     * onTOuchMethod is called when user touches the display
+     */
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
+    }
+//    When user first touches the screen
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+
+    private fun touchUp() {
+
+    }
+
+    private fun touchMove() {
+
+    }
+
+
+
+}
